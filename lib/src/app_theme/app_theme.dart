5a200b409e8ee16a3aa8f9135bc8eabf3b560df1
Ogabek <ogabekabdijabborov@gmail.com>
2022-01-26 00:08:43 +0500

    00:08

--- lib/src/app_theme/app_theme.dart
+++ lib/src/app_theme/app_theme.dart
@@ -9,8 +9,11 @@ class AppTheme {
   static const Color grey = Color(0xFF96A0B5);
   static const Color dark = Color(0xFF121F3E);
   static const Color milk = Color(0xFFF5F6FF);
+  static const Color black6A  = Color(0xFF59616A);
   static const Color blue = Color(0xFF326CF8);
   static const Color black = Color(0xFF000000);
+  static const Color black9E = Color(0xFF9E9E9E);
+  static const Color orange = Color(0xFFFC5A1F);
 
   static const Color shimmerBaseColor = Color(0xE0E0E0FF);
   static const Color shimmerHighColor = Color(0xF5F5F5FF);
--- lib/src/ui/book_pages/book_pages_screen.dart
+++ lib/src/ui/book_pages/book_pages_screen.dart
@@ -1,6 +1,7 @@
 import 'package:book_ogabek/src/Utils/utils.dart';
 import 'package:book_ogabek/src/app_theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BookPagesScreen extends StatefulWidget {
   const BookPagesScreen({Key? key}) : super(key: key);
@@ -17,6 +18,8 @@ class _BookPagesScreenState extends State<BookPagesScreen> {
     double o = (h + w) / 2;
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
             height: 472 * h,
@@ -39,9 +42,10 @@ class _BookPagesScreenState extends State<BookPagesScreen> {
               right: 18 * w,
             ),
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Container(
-
                   child: Text(
                     "НИ СЫ. Будь уверен в своих силах и не позволяй сомнениям мешать двигаться тебе вперед",
                     style: TextStyle(
@@ -52,12 +56,108 @@ class _BookPagesScreenState extends State<BookPagesScreen> {
                       height: 28 / 20 * h,
                     ),
                   ),
-                  height: 84*h,
-                  width: MediaQuery.of(context).size.width-83*w,
-
+                  height: 84 * h,
+                  width: MediaQuery.of(context).size.width - 83 * w,
                 ),
-
+                const Spacer(),
+                SvgPicture.asset(
+                  "assets/icons/mark.svg",
+                ),
               ],
+            ),
+          ),
+          Container(
+            height: 25 * h,
+            width: 135 * h,
+            margin: EdgeInsets.only(top: 16 * h, left: 16 * w),
+            child: Row(
+              children: [
+                Text(
+                  "Джен Синсеро",
+                  style: TextStyle(
+                    color: AppTheme.black6A,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16 * o,
+                  ),
+                ),
+                const Spacer(),
+                SvgPicture.asset("assets/icons/arrow_right.svg"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 24 * h,
+          ),
+          Container(
+            height: 24 * h,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 16 * w),
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/more.svg",
+                ),
+                SizedBox(
+                  width: 8 * w,
+                ),
+                Text(
+                  "Содержание",
+                  style: TextStyle(
+                    fontFamily: AppTheme.fontFamilyManrope,
+                    color: AppTheme.black6A,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16 * o,
+                  ),
+                ),
+                const Spacer(),
+                Text(
+                  "Длительность: 06:29:08",
+                  style: TextStyle(
+                    fontFamily: AppTheme.fontFamilyManrope,
+                    color: AppTheme.black9E,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16 * o,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 56 * h,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.only(
+              top: 40 * h,
+              left: 17 * h,
+              right: 20 * w,
+            ),
+            decoration: BoxDecoration(
+              color: AppTheme.orange,
+              borderRadius: BorderRadius.circular(8 * o),
+            ),
+            child: Center(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset("assets/icons/play.svg"),
+                  SizedBox(
+                    width: 10 * w,
+                  ),
+                  Text(
+                    "Слушать",
+                    style: TextStyle(
+                      fontFamily: AppTheme.fontFamilyManrope,
+                      color: AppTheme.white,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16 * o,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
