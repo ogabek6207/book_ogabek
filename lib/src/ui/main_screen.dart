af44ef45bd69565b372f46a8e1f5283bdbb0ba0a
Ogabek <ogabekabdijabborov@gmail.com>
2022-01-26 21:30:02 +0500

    21:29

--- lib/src/ui/main_screen.dart
+++ lib/src/ui/main_screen.dart
@@ -5,9 +5,6 @@ import 'package:book_ogabek/src/ui/tab_bar/tabbar_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import 'company/news_screen.dart';
-import 'home/genres_screen.dart';
-
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
@@ -22,11 +19,10 @@ class _MainScreenState extends State<MainScreen> {
 
   List<Widget> data = [
     const TabBarScreen(),
-     SaveBookScreen(),
+    const SaveBookScreen(),
     const SettingScreen(),
   ];
 
-  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +35,7 @@ class _MainScreenState extends State<MainScreen> {
           });
         },
         type: BottomNavigationBarType.fixed,
-        unselectedLabelStyle: const TextStyle(
-          color: Colors.red,
-          fontSize: 14,
-          fontWeight: FontWeight.normal,
-        ),
-        selectedItemColor: AppTheme.purple,
+        selectedItemColor: AppTheme.orange,
         items: [
           BottomNavigationBarItem(
             icon: _selectedIndex == 0
@@ -56,7 +47,6 @@ class _MainScreenState extends State<MainScreen> {
             icon: _selectedIndex == 1
                 ? SvgPicture.asset(
                     "assets/icons/player.svg",
-
                   )
                 : SvgPicture.asset(
                     "assets/icons/player1.svg",
@@ -67,7 +57,6 @@ class _MainScreenState extends State<MainScreen> {
             icon: _selectedIndex == 2
                 ? SvgPicture.asset(
                     "assets/icons/my_books.svg",
-
                   )
                 : SvgPicture.asset(
                     "assets/icons/my_books1.svg",
--- lib/src/ui/save_book/save_book_screen.dart
+++ lib/src/ui/save_book/save_book_screen.dart
@@ -1,21 +1,14 @@
-import 'package:audioplayers/audioplayers.dart';
-import 'package:audioplayers/web/audioplayers_web.dart';
 import 'package:flutter/material.dart';
 
 class SaveBookScreen extends StatefulWidget {
-   SaveBookScreen({Key? key}) : super(key: key);
+  const SaveBookScreen({Key? key}) : super(key: key);
 
   @override
   _SaveBookScreenState createState() => _SaveBookScreenState();
 }
 
 class _SaveBookScreenState extends State<SaveBookScreen> {
-  AudioPlayer audioPlayer = AudioPlayer();
-  String url = "http://www.antiqa.uz/load/yangi_mp3lar_musiqalar_muzikalar/o_39_zbek_mp3_o_39_zbekcha_taronalar_2016_2017/davron_ergashev_rohman_ya_rohman/2-1-0-1281";
   @override
-
-
-
   Widget build(BuildContext context) {
     return Scaffold();
   }
